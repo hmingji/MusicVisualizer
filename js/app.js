3d58f99bf7885fb442ddfeb64677758d1c6f5723
hmingji <hmingji@hotmail.com>
2022-07-24 15:58:05 +0800

    add wavepath toggle feature

--- js/app.js
+++ js/app.js
@@ -1,5 +1,5 @@
 const audioFileDropzone = document.querySelector('.audiofile-dropzone');
-const audio = document.querySelector('#audio');
+const audio = document.querySelector('#audio'); 
 const audioFileInput = document.querySelector('#audio-file');
 
 audioFileDropzone.addEventListener('dragenter', (e) => {
@@ -69,13 +69,32 @@ document.addEventListener('drop', (e) => e.preventDefault());
 
 const audioCtx = new AudioContext();
 const audioCanvas = document.getElementById("audio-visualizer-canvas");
+
+/** @type {HTMLCanvasElement} */
+const baseCanvas = document.getElementById('base-canvas');
+
 const audioPlayButton = document.querySelector('#audioplay-button');
+
 audioCanvas.height = window.innerHeight;
 audioCanvas.width = window.innerWidth;
 const audioCanvasCtx = audioCanvas.getContext('2d');
-console.log(audioCanvasCtx)
+
+baseCanvas.height = window.innerHeight;
+baseCanvas.width = window.innerWidth;
+const baseCanvasCtx = baseCanvas.getContext('2d');
+
 let audioSource;
 let audioAnalyser;
+var wavePathOption = "horizontal";
+
+const wavePathButtons = document.querySelectorAll('input[name="wavepath"]');
+
+for (const wavePathButton of wavePathButtons) {
+    wavePathButton.addEventListener('change', function(event){
+        if (this.checked) wavePathOption = this.value;
+        return;
+    })
+}
 
 audioPlayButton.addEventListener('click', (e) => {
     audioCtx.resume();
@@ -90,38 +109,86 @@ audioPlayButton.addEventListener('click', (e) => {
     audioAnalyser.maxDecibels = -10;
     const bufferLength = audioAnalyser.frequencyBinCount;
     const dataArray = new Uint8Array(bufferLength);
-    console.log(dataArray);
-    const barWidth = audioCanvas.width/(bufferLength)*0.75;
+    const widthOffset = audioCanvas.width * 0.25 / 2;
+    const heightOffset = 50;
+    const baseHeight = 12;
+    const baseWidth = audioCanvas.width * 0.75;
+    const dataArrayOffset = 10;
+    const barWidth = audioCanvas.width / (bufferLength + 10) * 0.75;
     let barHeight;
     let x;
 
+    isRunning = !isRunning;
 
-    function animate() {
+    function drawBase(displayOption) {
+        if (displayOption === "circular") {
+            baseCanvasCtx.beginPath();
+            baseCanvasCtx.lineWidth = 20;
+            baseCanvasCtx.strokeStyle = '#40e0d0';
+            baseCanvasCtx.arc(baseCanvas.width / 2, baseCanvas.height / 2, 190, 0, 2 * Math.PI);
+            baseCanvasCtx.stroke();
+        } else {
+            baseCanvasCtx.beginPath();
+            baseCanvasCtx.lineWidth = 20;
+            baseCanvasCtx.strokeStyle = '#40e0d0';
+            baseCanvasCtx.moveTo(baseCanvas.width*0.25/2, baseCanvas.height - heightOffset - 20);
+            baseCanvasCtx.lineTo(baseCanvas.width*(0.25/2+0.75), baseCanvas.height - heightOffset - 20);
+            baseCanvasCtx.stroke();
+        }
+    }
+
+    function horizontalAnimate() {
         x = 0;
         audioCanvasCtx.clearRect(0, 0, audioCanvas.width, audioCanvas.height);
         audioAnalyser.getByteFrequencyData(dataArray);
-        audioCanvasCtx.fillStyle = '#40e0d0';
-        audioCanvasCtx.fillRect(audioCanvas.width*0.25/2, audioCanvas.height - 10 - 20, audioCanvas.width*0.75, 10);
+       
+        for (let i = 0; i < bufferLength + dataArrayOffset; i++){
+            (i < dataArrayOffset) ? barHeight = 0 : barHeight = dataArray[i - dataArrayOffset];
 
-        for (let i = 0; i < bufferLength; i++){
-            barHeight = dataArray[i];
-
-            let grd = audioCanvasCtx.createLinearGradient(x + audioCanvas.width*0.25/2, audioCanvas.height - 10 - 20, x + audioCanvas.width*0.25/2, audioCanvas.height - barHeight - 10 - 20);
-            grd.addColorStop(0, "#40e0d0");
-            grd.addColorStop(0.5, "#ff8c00");
-            grd.addColorStop(1, "#ff0080");
-            audioCanvasCtx.fillStyle = grd;
+            audioCanvasCtx.fillStyle = "#40e0d0";
+            audioCanvasCtx.fillRect(x + widthOffset, audioCanvas.height - barHeight - baseHeight - heightOffset, barWidth, barHeight);
             
-            audioCanvasCtx.fillRect(x + audioCanvas.width*0.25/2, audioCanvas.height - barHeight - 10 - 20, barWidth, barHeight);
-            
-            //audioCanvasCtx.fillStyle = '#ff8c00';
-            //audioCanvasCtx.fillRect(x + audioCanvas.width*0.25/2, audioCanvas.height - (barHeight*0.5) - 10 - 20, barWidth, barHeight*0.5);
             x += barWidth;
         }
-        //need to add another canvas to cover the rounded bottom
-        //audioCanvasCtx.clearRect(audioCanvas.width*0.25/2, audioCanvas.height - 20 - 10 - 20, audioCanvas.width*(0.25/2+0.75), 30);
-        requestAnimationFrame(animate);
+        
+        requestAnimationFrame(horizontalAnimate);
+
     }
-    animate();
+
+    function circularAnimate() {
+        x = 0;        
+        audioCanvasCtx.clearRect(0, 0, audioCanvas.width, audioCanvas.height);
+        audioAnalyser.getByteFrequencyData(dataArray);
+
+        for (let i = 0; i < bufferLength + dataArrayOffset; i++){
+            (i < dataArrayOffset) ? barHeight = 0 : barHeight = dataArray[i - dataArrayOffset];
+            
+            audioCanvasCtx.save();
+            audioCanvasCtx.translate(audioCanvas.width / 2, audioCanvas.height / 2);
+            audioCanvasCtx.rotate(i * Math.PI * 2 / (bufferLength + dataArrayOffset));
+            
+            audioCanvasCtx.fillStyle = "#40e0d0";
+            audioCanvasCtx.fillRect(0, 190, barWidth, barHeight);
+
+            x += barWidth;
+            audioCanvasCtx.restore();
+        }
+        
+        requestAnimationFrame(circularAnimate);
+    }
+
+    if (wavePathOption === "horizontal") {
+        drawBase("horizontal");
+        horizontalAnimate();
+        return;
+    }
+
+    if (wavePathOption === "circular") {
+        drawBase("circular");
+        circularAnimate();
+        return;
+    }
+
+    return;
 })
 
